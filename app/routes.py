2225055a30caa1a1e1cfcf642b3db0f97e438b6f
Jodi Denney <iarrived@gmail.com>
2022-04-29 08:15:49 -0700

    GEt a single book

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, abort, make_response, jsonify
+
 
 class Book:
     def __init__(self, id, title, description):
         self.id = id
         self.title = title
         self.description = description
+
 
 books = [
     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
@@ -14,6 +16,7 @@ books = [
 ]
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
 
 @books_bp.route("", methods=["GET"])
 def handle_books():
@@ -27,3 +30,27 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message": f"book {book_id} not found"}, 404))
+
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
