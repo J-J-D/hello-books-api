bdcf549864648a424bb6616cbd61a58383837173
Jodi Denney <iarrived@gmail.com>
2022-04-30 09:36:38 -0700

    Get all route

--- app/routes.py
+++ app/routes.py
@@ -18,18 +18,30 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Book {new_book.title} successfully created", 201)
 
-#def validate_book(book_id):
+# def validate_book(book_id):
 #    try:
 #        book_id = int(book_id)
 #    except:
@@ -40,7 +52,7 @@ def handle_books():
 #            return book
 #
 #    abort(make_response({"message":f"book {book_id} not found"}, 404))
-        
+
 
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
@@ -64,4 +76,3 @@ def handle_books():
 #           "title": book.title,
 #           "description": book.description,
 #     }
-
